f8c426dacb37f9e585f449615a846af9fdaf4a63
Emily Lam <hlam93@gmail.com>
2017-10-11 13:03:50 -0700

    Create tensorflow.py

---
+++ tensorflow.py
@@ -0,0 +1,138 @@
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import sys
+import tempfile
+
+import pandas as pd
+from six.moves import urllib
+import tensorflow as tf
+
+
+CSV_COLUMNS = [
+    "food", "glucose_num"
+]
+
+
+# Continuous base columns.
+food = tf.feature_column.numeric_column("food")
+glucose = tf.feature_column.numeric_column("glucose_num")
+
+# Transformations.
+glucose_buckets = tf.feature_column.bucketized_column(
+    glucose, boundaries=[60,80,100,120,140,160,180,200,220,240])
+
+# Wide columns and deep columns.
+base_columns = [
+    food
+]
+
+crossed_columns = [
+    tf.feature_column.crossed_column(
+        ["food", "glucose"], hash_bucket_size=1000),
+]
+
+def build_estimator(model_dir, model_type):
+  """Build an estimator."""
+  if model_type == "wide":
+    m = tf.estimator.LinearClassifier(
+        model_dir=model_dir, feature_columns=base_columns + crossed_columns)
+  elif model_type == "deep":
+    m = tf.estimator.DNNClassifier(
+        model_dir=model_dir,
+        feature_columns=deep_columns,
+        hidden_units=[100, 50])
+  else:
+    m = tf.estimator.DNNLinearCombinedClassifier(
+        model_dir=model_dir,
+        linear_feature_columns=crossed_columns,
+        dnn_feature_columns=deep_columns,
+        dnn_hidden_units=[100, 50])
+  return m
+
+
+def input_fn(data_file, num_epochs, shuffle):
+  """Input builder function."""
+  df_data = pd.read_csv(
+      tf.gfile.Open(data_file),
+      names=CSV_COLUMNS,
+      skipinitialspace=True,
+      engine="python",
+      skiprows=1)
+  # remove NaN elements
+  df_data = df_data.dropna(how="any", axis=0)
+  labels = df_data["income_bracket"].apply(lambda x: ">50K" in x).astype(int)
+  return tf.estimator.inputs.pandas_input_fn(
+      x=df_data,
+      y=labels,
+      batch_size=100,
+      num_epochs=num_epochs,
+      shuffle=shuffle,
+      num_threads=5)
+
+
+def train_and_eval(model_dir, model_type, train_steps, train_data, test_data):
+  """Train and evaluate the model."""
+  train_file_name, test_file_name = maybe_download(train_data, test_data)
+  model_dir = tempfile.mkdtemp() if not model_dir else model_dir
+
+  m = build_estimator(model_dir, model_type)
+  # set num_epochs to None to get infinite stream of data.
+  m.train(
+      input_fn=input_fn(train_file_name, num_epochs=None, shuffle=True),
+      steps=train_steps)
+  # set steps to None to run evaluation until all data consumed.
+  results = m.evaluate(
+      input_fn=input_fn(test_file_name, num_epochs=1, shuffle=False),
+      steps=None)
+  print("model directory = %s" % model_dir)
+  for key in sorted(results):
+    print("%s: %s" % (key, results[key]))
+
+
+FLAGS = None
+
+
+def main(_):
+  train_and_eval(FLAGS.model_dir, FLAGS.model_type, FLAGS.train_steps,
+                 FLAGS.train_data, FLAGS.test_data)
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.register("type", "bool", lambda v: v.lower() == "true")
+  parser.add_argument(
+      "--model_dir",
+      type=str,
+      default="",
+      help="Base directory for output models."
+  )
+  parser.add_argument(
+      "--model_type",
+      type=str,
+      default="wide_n_deep",
+      help="Valid model types: {'wide', 'deep', 'wide_n_deep'}."
+  )
+  parser.add_argument(
+      "--train_steps",
+      type=int,
+      default=2000,
+      help="Number of training steps."
+  )
+  parser.add_argument(
+      "--train_data",
+      type=str,
+      default="",
+      help="Path to the training data."
+  )
+  parser.add_argument(
+      "--test_data",
+      type=str,
+      default="",
+      help="Path to the test data."
+  )
+  FLAGS, unparsed = parser.parse_known_args()
+  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
