39344735187f4dd7f86eb0b68e1c0963dbfb0f93
Emily Lam <hlam93@gmail.com>
2017-10-11 10:01:45 -0700

    Update morningDose.js

--- morningDose.js
+++ morningDose.js
@@ -1,8 +1,8 @@
 var library = (function () {
 
-    var appName = 'MorningDose';
+    var appName = 'Diatek';
     var welcomeText = "Welcome to " + appName;
-    var helpText = "You can say, tell me about my meetings, emails, , or github. You can also ask: read me my new messages";
+    var helpText = "You can say, log food, monitor glucose level, or tell me something about my health.";
     var exitText = 'Closed ' + appName;
     var subjectHelp = "Sorry, I didn't get that. Say tell me about github, email, or meetings.";
     var settingHelp = "Sorry, I didn't get that. Say configure workspace";
